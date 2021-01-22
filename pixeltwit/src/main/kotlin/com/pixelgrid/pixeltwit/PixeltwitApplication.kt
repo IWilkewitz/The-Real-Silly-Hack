c84675277589820f3f5de0b69321a7ade75f7470
Carter Rice <carterlrice@gmail.com>
2021-01-21 22:16:03 -0500

    Backend SpringBoot in Kotlin, this is the staging code

---
+++ pixeltwit/src/main/kotlin/com/pixelgrid/pixeltwit/PixeltwitApplication.kt
@@ -0,0 +1,11 @@
+package com.pixelgrid.pixeltwit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PixeltwitApplication
+
+fun main(args: Array<String>) {
+	runApplication<PixeltwitApplication>(*args)
+}
---
+++ pixeltwit/src/test/kotlin/com/pixelgrid/pixeltwit/PixeltwitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pixelgrid.pixeltwit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PixeltwitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
